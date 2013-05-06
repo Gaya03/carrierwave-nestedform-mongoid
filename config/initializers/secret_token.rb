6703bdae200eedec98c6f3d136ccf95898210f27
hzmangel <hzmangel@gmail.com>
2013-05-06 22:38:10 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveNestedformMongoid::Application.config.secret_token = '6db91d97769ea9e82dc8eeb4f5fe52797ec8352311db1a724957cf10ec0ba470f644c43233e4e09a297802ac14379d25248e828ab44121d93c6cb39a51a32340'
