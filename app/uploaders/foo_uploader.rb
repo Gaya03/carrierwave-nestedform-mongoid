e58e62270893bf27b9c0a64b45a2f396be44abcc
hzmangel <hzmangel@gmail.com>
2013-05-07 23:21:37 +0800

    Finish the demo

--- app/uploaders/foo_uploader.rb
+++ app/uploaders/foo_uploader.rb
@@ -4,11 +4,11 @@ class FooUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -29,7 +29,7 @@ class FooUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [800, 600]
+  process :resize_to_fill => [800, 600]
   #
   # def scale(width, height)
   #   # do something
@@ -37,7 +37,7 @@ class FooUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :scale => [80, 60]
+    process :resize_to_fill => [80, 60]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
